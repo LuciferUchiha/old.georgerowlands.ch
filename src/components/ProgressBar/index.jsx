3bdcb7fec982ed1f7e34e5067e8093393718b2a7
Lucifer Uchiha <rowla101@gmail.com>
2022-03-23 15:12:24 +0100

    starting progress bar new

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -4,49 +4,13 @@ import styled from "styled-components";
 /* Yes a progress bar could also be done with css but I wanted to try a different approach 
 and liked the look of the one using Unicodes in the Fira Code font*/
 const ProgressBar = ({ label, length }) => {
-  const getEmptyProgressString = (length) =>
-    "\uEE00" + "\uEE01".repeat(length) + "\uEE02";
-
   let [currentProgress, setCurrentProgress] = useState(0);
-  const [progressString, setProgressString] = useState(
-    getEmptyProgressString(length)
-  );
-
-  const updateProgressString = ({
-    length,
-    currentProgress,
-    progressString,
-  }) => {
-    console.log(currentProgress);
-    console.log(progressString);
-    setCurrentProgress(currentProgress % (length + 2)); // make sure we dont overflow: ;
-    if (currentProgress == 0) setProgressString(getEmptyProgressString(length));
-    else {
-      if (currentProgress == 1)
-        setProgressString(
-          "\uEE04" + progressString.substring(1, progressString.length - 1)
-        );
-      else if (currentProgress == length + 2)
-        setProgressString(
-          progressString.substring(0, progressString.length - 2) + "\uEE06"
-        );
-      else
-        setProgressString(
-          progressString.substring(currentProgress - 1) +
-            "\uEE05" +
-            progressString.substring(
-              currentProgress + 1,
-              progressString.length - 1
-            )
-        );
-    }
-    setCurrentProgress(currentProgress + 1); // increment
-  };
 
   useEffect(() => {
-    currentProgress = 0;
     const interval = setInterval(() => {
-      updateProgressString(length, currentProgress, progressString);
+      setCurrentProgress(
+        (currentProgress) => (currentProgress + 1) % (length + 3)
+      );
     }, 1000);
     return () => clearInterval(interval);
   }, []);
@@ -54,7 +18,7 @@ const ProgressBar = ({ label, length }) => {
   return (
     <Container>
       <Label>{label}</Label>
-      <Bar>{progressString}</Bar>
+      <Bar>{currentProgress}</Bar>
     </Container>
   );
 };
