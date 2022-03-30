c803d70aea9a7ae305187e2e1f2804035117c65b
Lucifer Uchiha <rowla101@gmail.com>
2022-03-30 09:55:01 +0200

    Cleaned up box.js

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -1,31 +1,37 @@
 import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 import { useFrame } from '@react-three/fiber';
 
-function Box(props) {
+function Box({ position }) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef();
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
+  useFrame(() => {
     ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
-      {...props}
+      position={position}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   );
 }
+
+// TODO: check if array has length 3 but does it always?
+Box.propTypes = {
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
 
 export default Box;
