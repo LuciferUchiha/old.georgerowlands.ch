e29040bc44f05b95030debee6afbea2fa4ca2ef9
George R <rowla101@gmail.com>
2022-08-29 15:07:54 +0900

    Changed progress bar to css animation for smoother loading

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,46 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 /* Yes a progress bar could also be done with css but I wanted to try a different approach
 and liked the look of the one using Unicodes in the Fira Code font */
-function ProgressBar({ label, length }) {
-  const [counters, setCounters] = useState([0, 0]);
-
-  const [spinnerCounter, progressCounter] = counters;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounters(() => [
-        (spinnerCounter + 1) % 6,
-        (progressCounter + 1) % (length + 3),
-      ]);
-    }, 200);
-    return () => clearInterval(interval);
-  }, [length, progressCounter, spinnerCounter]);
-
-  const spinnerString = String.fromCodePoint(
-    parseInt(`EE0${(spinnerCounter + 6).toString(16)}`, 16),
-  );
-
-  let progressString = '';
-  if (progressCounter === 0) {
-    progressString = `\uEE00${'\uEE01'.repeat(length + 1)}\uEE02`; // shouldnt be + 1 but it works
-  } else {
-    progressString = `\uEE03${
-      '\uEE04'.repeat(progressCounter - 1)
-    }${'\uEE01'.repeat(length - (progressCounter - 2))}`;
-    if (progressCounter === length + 2) progressString += '\uEE05';
-    else progressString += '\uEE02';
-  }
-
+function ProgressBar({ label }) {
   return (
     <Container>
-      <Label>{label}</Label>
-      <FiraCode>
-        {progressString}
-        {` ${spinnerString}`}
-      </FiraCode>
+      <h2>{label}</h2>
+      <ProgressOuter>
+        <ProgressInner />
+      </ProgressOuter>
     </Container>
   );
 }
@@ -49,23 +19,31 @@ export default ProgressBar;
 
 ProgressBar.propTypes = {
   label: PropTypes.string.isRequired,
-  length: PropTypes.number.isRequired,
 };
 
-const Label = styled.div`
-  margin-right: 10px;
-`;
-
 const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  font-size: 1.5rem;
   color: var(--ifm-color-primary-light);
 `;
 
-const FiraCode = styled.div`
-  padding: 5px 0px 0px;
-  font-family: "Fira Code VF", monospace;
-  letter-spacing: -0.02rem; // because otherwise weird line
+const progress = keyframes`
+  0% {
+    width: 0%;
+    background: #3700b3;
+  }
+
+  100% {
+    width: 100%;
+    background: #985eff;
+  }
+`;
+
+const ProgressOuter = styled.div`
+  border-radius: 30px;
+  animation: ${progress} 5s infinite;
+`;
+
+const ProgressInner = styled.div`
+  height: 18px;
+  border-radius: 30px;
+  animation: ${progress} 5s infinite;
 `;
