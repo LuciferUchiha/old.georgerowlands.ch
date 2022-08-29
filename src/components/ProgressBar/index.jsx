b401e8f4b741e8f06a23c77686c6e51460eb9b34
George R <rowla101@gmail.com>
2022-08-29 15:37:54 +0900

    Cleaned up progress bar

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 
-/* Yes a progress bar could also be done with css but I wanted to try a different approach
-and liked the look of the one using Unicodes in the Fira Code font */
 function ProgressBar({ label }) {
   return (
-    <Container>
-      <h2>{label}</h2>
-      <ProgressOuter>
-        <ProgressInner />
-      </ProgressOuter>
-    </Container>
+    <>
+      <Label>{label}</Label>
+      <Bar />
+    </>
   );
 }
 
@@ -21,29 +17,23 @@ ProgressBar.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
-const Container = styled.div`
-  color: var(--ifm-color-primary-light);
+const Label = styled.h2`
+  color: var(--ifm-color-primary);
 `;
 
 const progress = keyframes`
   0% {
     width: 0%;
-    background: #3700b3;
   }
 
   100% {
     width: 100%;
-    background: #985eff;
   }
 `;
 
-const ProgressOuter = styled.div`
-  border-radius: 30px;
-  animation: ${progress} 5s infinite;
-`;
-
-const ProgressInner = styled.div`
+const Bar = styled.div`
   height: 18px;
   border-radius: 30px;
+  background: #985eff;
   animation: ${progress} 5s infinite;
 `;
