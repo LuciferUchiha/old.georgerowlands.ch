ae1508efe0790b6ec69797ba003b8bd81a6306d8
George R <rowla101@gmail.com>
2022-09-11 21:48:13 +0900

    Jupyter should work

---
+++ docs/Computer Science/Artificial Intelligence/myscript.py
@@ -0,0 +1,8 @@
+
+class MyClass:
+    @classmethod
+    def run(cls):
+        return "hello world"
+
+if __name__ == "__main__":
+    print(MyClass.run())
---
+++ src/components/CodeOutputBlock/CodeOutputBlock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './CodeOutputBlock.module.css';
+
+/**
+ * Wrapper for code output blocks
+ */
+export const CodeOutputBlock = ({ children }) => <div className={styles.wrapper}>{children} </div>;
+
+export default CodeOutputBlock;
