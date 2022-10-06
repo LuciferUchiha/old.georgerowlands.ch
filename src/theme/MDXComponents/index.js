4231849f6fbc219d1fa1a15541e6b298a0f1a02f
George R <rowla101@gmail.com>
2022-10-06 15:19:14 +0900

    Fixed admonitions

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -4,16 +4,17 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, { isValidElement } from "react";
-import Head from "@docusaurus/Head";
-import Link from "@docusaurus/Link";
-import CodeBlock from "@theme/CodeBlock";
-import Heading from "@theme/Heading";
-import Details from "@theme/Details";
-import "./styles.css"; // MDX elements are wrapped through the MDX pragma
+import React, { isValidElement } from 'react';
+import Head from '@docusaurus/Head';
+import Link from '@docusaurus/Link';
+import CodeBlock from '@theme/CodeBlock';
+import Heading from '@theme/Heading';
+import Details from '@theme/Details';
+import Admonition from '@theme/Admonition';
+import './styles.css'; // MDX elements are wrapped through the MDX pragma
 // In some cases (notably usage with Head/Helmet) we need to unwrap those elements.
-import { CodeOutputBlock } from "/src/components/CodeOutputBlock/CodeOutputBlock";
-import { HTMLOutputBlock } from "/src/components/HTMLOutputBlock/HTMLOutputBlock";
+import { CodeOutputBlock } from '/src/components/CodeOutputBlock/CodeOutputBlock';
+import { HTMLOutputBlock } from '/src/components/HTMLOutputBlock/HTMLOutputBlock';
 
 function unwrapMDXElement(element) {
   if (element?.props?.mdxType && element?.props?.originalType) {
@@ -33,15 +34,14 @@ const MDXComponents = {
   },
   code: (props) => {
     const shouldBeInline = React.Children.toArray(props.children).every(
-      (el) => typeof el === "string" && !el.includes("\n")
+      (el) => typeof el === 'string' && !el.includes('\n')
     );
     return shouldBeInline ? <code {...props} /> : <CodeBlock {...props} />;
   },
   a: (props) => <Link {...props} />,
   pre: (props) => (
     <CodeBlock // If this pre is created by a ``` fenced codeblock, unwrap the children
-      {...(isValidElement(props.children) &&
-      props.children.props.originalType === "code"
+      {...(isValidElement(props.children) && props.children.props.originalType === 'code'
         ? props.children?.props
         : { ...props })}
     />
@@ -49,7 +49,7 @@ const MDXComponents = {
   details: (props) => {
     const items = React.Children.toArray(props.children); // Split summary item from the rest to pass it as a separate prop to the Details theme component
 
-    const summary = items.find((item) => item?.props?.mdxType === "summary");
+    const summary = items.find((item) => item?.props?.mdxType === 'summary');
     const children = <>{items.filter((item) => item !== summary)}</>;
     return (
       <Details {...props} summary={summary}>
@@ -63,6 +63,7 @@ const MDXComponents = {
   h4: (props) => <Heading as="h4" {...props} />,
   h5: (props) => <Heading as="h5" {...props} />,
   h6: (props) => <Heading as="h6" {...props} />,
+  admonition: Admonition,
   CodeOutputBlock,
   HTMLOutputBlock,
 };
